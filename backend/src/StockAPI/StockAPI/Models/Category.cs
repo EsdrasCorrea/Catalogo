c6e0d757296e9995342262a030c085cbf7a46e08
EsdrasCorrea <esdras_correa@hotmail.com>
2025-10-24 19:36:24 +0100

    chore: clean template controllers

---
+++ backend/src/StockAPI/StockAPI/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace StockAPI.Models;
+
+public class Category
+{
+    public int CategoryId { get; set; }
+    public string? Name { get; set; }
+    public string? ImagemUrl { get; set; }
+}
